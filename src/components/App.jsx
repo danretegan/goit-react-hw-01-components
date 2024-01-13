bb6e9b50060218304eccec6c647cdc8c90cf9705
Dan Retegan <danretegan@yahoo.com>
2024-01-13 12:27:39 +0200

    Profile

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,22 +1,27 @@
+import React from 'react';
 import Profile from './profile/Profile';
 import data from './user.json';
 
 export const App = () => {
-  console.log(data);
   return (
     <div
       style={{
-        height: '100vh',
+        // height: '100vh',
         display: 'flex',
         flexDirection: 'column',
         justifyContent: 'center',
         alignItems: 'center',
-        fontSize: 40,
-        color: '#010101',
+        // fontSize: 40,
+        // color: '#010101',
       }}
     >
-      React homework template Test
-      <Profile condition={true}></Profile>
+      <Profile
+        username={data.username}
+        tag={data.tag}
+        location={data.location}
+        avatar={data.avatar}
+        stats={data.stats}
+      />
     </div>
   );
 };
--- src/components/profile/Profile.jsx
+++ src/components/profile/Profile.jsx
@@ -1,18 +1,47 @@
-import styles from './Profile.module.css';
+import React from 'react';
 import PropTypes from 'prop-types';
+import styles from './Profile.module.css';
 
-const Profile = ({ condition }) => {
+const Profile = ({ username, tag, location, avatar, stats }) => {
+  const { followers, views, likes } = stats;
+
   return (
-    <div className={styles.black}>
-      {condition && <h2>Titlu</h2>}
-      {condition ? <h2>Cu titlu</h2> : <h2>Fara titlu</h2>}
-      <p>Profile Page</p>
+    <div className={styles.profile}>
+      <div className={styles.description}>
+        <img src={avatar} alt="User avatar" className={styles.avatar} />
+        <p className={styles.name}>{username}</p>
+        <p className={styles.tag}>{tag}</p>
+        <p className={styles.location}>{location}</p>
+      </div>
+
+      <ul className={styles.stats}>
+        <li>
+          <span className={styles.label}>followers: </span>
+          <span className={styles.quantity}>{followers}</span>
+        </li>
+        <li>
+          <span className={styles.label}>views: </span>
+          <span className={styles.quantity}>{views}</span>
+        </li>
+        <li>
+          <span className={styles.label}>likes: </span>
+          <span className={styles.quantity}>{likes}</span>
+        </li>
+      </ul>
     </div>
   );
 };
 
 Profile.propTypes = {
-  condition: PropTypes.bool,
+  username: PropTypes.string.isRequired,
+  tag: PropTypes.string.isRequired,
+  location: PropTypes.string.isRequired,
+  avatar: PropTypes.string.isRequired,
+  stats: PropTypes.shape({
+    followers: PropTypes.number.isRequired,
+    views: PropTypes.number.isRequired,
+    likes: PropTypes.number.isRequired,
+  }).isRequired,
 };
 
 export default Profile;
