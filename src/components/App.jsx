0b978ff76607f353fa1f48dc54c9b4a767060716
Dan Retegan <danretegan@yahoo.com>
2024-01-14 18:55:27 +0200

    Transaction History setup

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,27 @@
 import React from 'react';
+
 import Profile from './profile/Profile';
 import user from './user.json';
+
 import Statistics from './statistics/Statistics';
 import data from './data.json';
+
 import FriendList from './friendList/FriendList';
 import friends from './friends.json';
 
+import TransactionHistory from './transactionHistory/TransactionHistory';
+import transactions from './transactions.json';
+
 export const App = () => {
+  // Convertește valorile din "amount" la numere:
+  const transactionsWithNumberAmount = transactions.map(transaction => ({
+    ...transaction,
+    amount: Number(transaction.amount),
+  }));
+
   return (
     <div
       style={{
-        // height: '100vh',
         display: 'flex',
         flexDirection: 'column',
         justifyContent: 'center',
@@ -18,8 +29,6 @@ export const App = () => {
         margin: 20,
         padding: 20,
         backgroundColor: 'aliceblue',
-        // fontSize: 40,
-        // color: '#010101',
       }}
     >
       <Profile
@@ -31,6 +40,7 @@ export const App = () => {
       />
       <Statistics title="Upload stats" stats={data} />
       <FriendList friends={friends} />
+      <TransactionHistory items={transactionsWithNumberAmount} />
     </div>
   );
 };
